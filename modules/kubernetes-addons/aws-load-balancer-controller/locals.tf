135295dc077dcb65edf8d1b0ea5898598296e1e5
Renê Michels Rodrigues <rene.michels@accenture.com>
2022-08-31 22:37:13 -0300

    fix(addons/aws-load-balancer-controller): reduce service_account_name to avoid the error:
    expected length of name to be in the range (1 - 64)

--- modules/kubernetes-addons/aws-load-balancer-controller/locals.tf
+++ modules/kubernetes-addons/aws-load-balancer-controller/locals.tf
@@ -1,6 +1,6 @@
 locals {
   name            = "aws-load-balancer-controller"
-  service_account = try(var.helm_config.service_account, "${local.name}-sa")
+  service_account = "aws-lb-controller-sa"
 
   # https://github.com/aws/eks-charts/blob/master/stable/aws-load-balancer-controller/Chart.yaml
   default_helm_config = {
